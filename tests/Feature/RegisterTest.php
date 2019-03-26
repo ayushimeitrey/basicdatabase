97d3dd6d8587ea8ef744aacfa377144d32889fe1
ayushi <am2862@njit.edu>
2019-03-26 18:12:20 -0400

    Commit Post controller

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testRegisterPage()
+    {
+        //$response = $this->get('/register');
+        //$response->assertSuccessful();
+        //$response->assertOk();
+        //$status=$response->status();
+        //dd($response->status());
+        //$response->assertStatus(200);
+        $this->assertTrue(true);
+
+    }
+}
